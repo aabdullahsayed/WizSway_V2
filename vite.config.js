61f8fe38a4adf24e82e1acf1ba2431698298b43b
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-18 22:57:00 +0600

    Configure vite.config.js base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/WizSway_V2/'  // your repo name
-})
+    base: '/',
+});
