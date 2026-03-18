c5e7e75da5ab26f86c2a77a53b60f6a416782745
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-18 20:12:40 +0600

    Add new component footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
 
 function App() {
 
     return(
 
-        <Header/>
+        <>
+            <Header/>
+            <Footer/>
+        </>
+
 
     );
 
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+function footer(){
+    return(
+
+        <>
+            <h2>This is the footer</h2>
+        </>
+    );
+}
+
+export default footer;
